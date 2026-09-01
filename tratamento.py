ba923b7253bfeeffb74e02f707eec187e6f8d551
Mateus dos Santos Cavalheiro <mateus_s_cavalheiro@estudante.sesisenai.org.br>
2026-09-01 11:21:58 -0300

    testing functions

--- tratamento.py
+++ tratamento.py
@@ -2,4 +2,29 @@ import pandas as pd
 
 dados = pd.read_csv("dados_sensores.csv")
 
-print(dados)
+media_temp = dados["temperatura"].mean()
+
+print("Temperatura média: ",round(media_temp,2),"°C")
+
+print("-" * 120)
+
+max_press= dados["pressao"].max()
+
+print("A maior pressão registrada foi de: ",round(max_press,2),"bar")
+
+print("-" * 120)
+
+min_prod = dados["producao"].min()
+
+print("A menor produção foi de: ",min_prod)
+
+print("-" * 120)
+
+dados_ordenados = dados.sort_values(by="producao", ascending=False)
+
+print("Ordenação por produção (maior para menor)")
+
+print("\n")
+print(dados_ordenados)
+
+print("-" * 120)
